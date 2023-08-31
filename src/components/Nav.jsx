638f1ec3ea77fa379f5824789368b185bd50b94c
Ajith <ajith@Venkats-MacBook-Pro.local>
2023-08-31 18:41:50 +0530

    test

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,18 +6,18 @@ import Store from "../assets/images/store.svg";
 const Nav = () => {
   return (
     <nav className="nav-wrapper">
-      <div className="nav-content">
+      <div className="nav-content"> 
         <ul className="list-styled">
           <li>
             <img src={Logo} alt="Apple" />
           </li>
           <li>
-            <a className="link-styled">Store</a>
+            <a className="link-styled">Store</a> 
           </li>
           <li>
             <a className="link-styled">Mac</a>
           </li>
-          <li>
+          <li> 
             <a className="link-styled">iPad</a>
           </li>
           <li>
